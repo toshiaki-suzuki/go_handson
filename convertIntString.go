8ff884b379cff974c738fa7e5aafbbbcbea93bee
akis <flaflabell@gmail.com>
2022-02-12 09:54:18 +0900

    文字列と整数の型変換

---
+++ convertIntString.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	i, err := strconv.Atoi("3")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(i)
+	a := strconv.Itoa(3)
+	fmt.Println(a)
+}
