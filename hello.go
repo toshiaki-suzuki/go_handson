ce904ed22fcc712c178cd182791b1a4f60e3afdf
akis <flaflabell@gmail.com>
2022-02-24 08:33:24 +0900

    複数返り値を持つ関数

--- hello.go
+++ hello.go
@@ -7,16 +7,13 @@ import (
 )
 
 func main() {
-	m := map[string]int{
-		"a": 100,
-		"b": 200,
-		"c": 300,
-	}
-	m["total"] = m["a"] + m["b"] + m["c"]
-
-	for k, v := range m {
-		fmt.Println(k+":", v)
-	}
+	m := []string{}
+	m, _ = push(m, "apple")
+	m, _ = push(m, "banana")
+	m, _ = push(m, "orange")
+	fmt.Println(m)
+	m, v := pop(m)
+	fmt.Println("get "+v+" ->", m)
 }
 
 func input(msg string) string {
@@ -24,4 +21,11 @@ func input(msg string) string {
 	fmt.Print(msg + ": ")
 	scanner.Scan()
 	return scanner.Text()
+}
+func push(a []string, v string) ([]string, int) {
+	return append(a, v), len(a)
+}
+
+func pop(a []string) ([]string, string) {
+	return a[:len(a)-1], a[len(a)-1]
 }
